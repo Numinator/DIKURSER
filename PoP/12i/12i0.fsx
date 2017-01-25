426c86d6adaeea6cfa1c2e5556439b7624ae82e3
Frederik M <frederik.mastratisi@hotmail.com>
2017-01-25 21:40:27 +0100

    Tilf kommentare

--- PoP/12i/12i0.fsx
+++ PoP/12i/12i0.fsx
@@ -7,7 +7,7 @@ let load s =
   let bmp = new Bitmap(Image.FromFile(s))
   let get256Shades x y = // of gray
     let c = bmp.GetPixel (x, y)
-    int(c.R + c.G + c.B) / 3
+    (int c.R + int c.G + int c.B) / 3
   [|for y in 0 .. bmp.Height-1 do for x in 0.. bmp.Width-1 -> get256Shades x y|]
 
 let putInBuckets numBucketWish bmp =
@@ -20,6 +20,7 @@ let putInBuckets numBucketWish bmp =
   buckets
 
 let drawHistogram s' p' (buckets : int []) =
+  // Creating some commen constants used in the nested functions
   let win = new Form ()
   win.Text <- "Historgram with "+sprintf "%i" (Array.length buckets)+" buckets"
   win.BackColor  <- Color.White
@@ -31,18 +32,21 @@ let drawHistogram s' p' (buckets : int []) =
                 Point (s - p, s - p); Point (s - p, p);
                 Point (p, p)|]
   let letterSize = 30 //pixel
-  // Translate from frame coordinates to client and inverts y-axsis
+ 
+  // Translates from frame coordinates to client and inverts y-axsis
   let f2C (x, y) = (x + p , s - (y + p))
   let xF2C x = x + p
   let yF2C y = s - (y + p)
   
-  //Calculate width of collums in pixel
+  //Calculates width of collums in pixel
   let length = Array.length buckets
-  let wCol   = (s - 2 * p) / length
-  //Calculate function for height of collums in pixel
+  let wCol   = (s - 2 * p) / length // 1) The width of the collums is constant
+  //Calculates function for height of collums in pixel
   let hMax   = Array.max buckets 
-  let hCol n = (n * (s - 2 * p)) / hMax
+  let hCol n = (n * (s - 2 * p)) / hMax // 1) The height is not
   
+  // Function that draws a singel collums to the screen. 
+  //For use with Array.iter2
   let drawCollum k n (e : PaintEventArgs) = 
   // number k collum with frequency of n shades of gray 
     let x' = xF2C <| k * wCol
@@ -51,11 +55,13 @@ let drawHistogram s' p' (buckets : int []) =
     let h' = hCol n
     e.Graphics.DrawRectangle (pen, x', y', w', h')
 
-  // has structure (pixel dist, (incrementor, exponent))
+  // has structure (pixel distance, (incrementor, exponent))
   let findIncrementY =
-    let firstDigits : int list = [1; 2; 5] //Must be sorted in non-decreasing
+    let possibleLeadingDigits : int list = [1; 2; 5] //Must be sorted in non-decreasing
     let minDist = double (letterSize * hMax) / double (s - 2 * p)
-
+    
+    // Finds the exponent a number would have if it was written in scientific
+    // notation. Supports negative exponents on postive numbers
     let findExp (d : double) =
       if d < 0.0 then invalidArg "d" "Findexp does not support negative numbers" 
       let rec findExp' n e = if n > 9 then findExp' (n / 10) (e + 1) else e
@@ -65,18 +71,22 @@ let drawHistogram s' p' (buckets : int []) =
         findExp' (int d) 0
     
     let exp = findExp minDist
-    let minFD = int (ceil (minDist / (10.0 ** (double exp))))
-    let chooseFirstDigit = 
+    //Use of ceil because minum leading digit must round up
+    let minLD = int (ceil (minDist / (10.0 ** (double exp))))
+    
+    //Is first possible leading digit that is bigger than minum, and the exp
+    let chooseLeadingDigit = 
       try 
-        let a = List.find (fun x -> minFD <= x) firstDigits
+        let a = List.find (fun x -> minLD <= x) possibleLeadingDigits
         (a, exp)
-      with
-      | _ -> (List.head firstDigits, exp + 1)
-    let FD = (double (fst chooseFirstDigit)) * 
-             (10.0 ** (double (snd chooseFirstDigit))) 
-    let distInPixel = (double (s - 2 * p) * FD) / double hMax
-    (distInPixel, chooseFirstDigit)
+      with 
+      | _ -> (List.head possibleLeadingDigits, exp + 1)
+    let LD = (double (fst chooseLeadingDigit)) * 
+             (10.0 ** (double (snd chooseLeadingDigit))) 
+    let distInPixel = (double (s - 2 * p) * LD) / double hMax
+    (distInPixel, chooseLeadingDigit)
   
+  // Draws a notch on the y-axis. For use with Array.iter   
   let drawYNotch k (e : PaintEventArgs) = // k'th notch 
     let dist = fst findIncrementY
 
@@ -87,6 +97,7 @@ let drawHistogram s' p' (buckets : int []) =
 
     e.Graphics.DrawLines (pen, points)
   
+  // Draws a number on the y-axis. For use with Array.iter
   let drawYText k (e : PaintEventArgs) =
     let dist = fst findIncrementY
     let incr = fst (snd findIncrementY)
@@ -97,7 +108,8 @@ let drawHistogram s' p' (buckets : int []) =
     let y = float32 (yF2C <| k * (int dist) + letterSize / 2)
 
     e.Graphics.DrawString (text, font, brush, x, y)
-  
+
+  // Draws the exponent on top-most left corner
   let drawExp (e : PaintEventArgs) =
     let exp = snd (snd findIncrementY)
 
@@ -107,6 +119,7 @@ let drawHistogram s' p' (buckets : int []) =
     let y = float32 (yF2C <| (s - p) - (p - letterSize))
 
     e.Graphics.DrawString (text, font, brush, x, y)
+  
   //Has structure (pixel dist, incrementor)
   let findIncrementX =
     let n = 50.0 // width of the text on the x-axis
@@ -118,7 +131,8 @@ let drawHistogram s' p' (buckets : int []) =
     let valIncrLen = List.find (fun x -> minMarkerLen <= x) niceIncrNumbers
     let pixPerIncr = valIncrLen / valPerPixel
     (pixPerIncr, valIncrLen)
-
+  
+  // Draws a notch on the x-axis. For use with Array.iter 
   let drawXNotch k (e : PaintEventArgs) =
     let dist = fst findIncrementX
 
@@ -128,7 +142,8 @@ let drawHistogram s' p' (buckets : int []) =
     let points = [|Point(x, y); Point(x , y')|]
 
     e.Graphics.DrawLines (pen, points)
-  
+    
+  // Draws a number on the x-axis. For use with Array.iter
   let drawXText k (e : PaintEventArgs) =
     let dist = fst findIncrementX
     let incr = snd findIncrementX
@@ -164,5 +179,7 @@ let drawHistogram s' p' (buckets : int []) =
   
 
 
-
-load "coins.jpg" |> putInBuckets 32 |> drawHistogram 1000 50
+// Instructions: Uncomment the histograf you want to see, 
+//               and add comments to the other
+//load "FriskFraDikuMemes.jpg" |> putInBuckets 256 |> drawHistogram 1000 50
+load "FriskFraDikuMemes.jpg" |> putInBuckets 35 |> drawHistogram 100 10
